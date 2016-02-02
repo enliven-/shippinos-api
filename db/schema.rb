44611e01e295c6745e4f0e42f63fb3a83bca9e11
Viksit Arora <viksit@mit.edu>
2016-02-03 01:18:53 +0530

    generates city model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202171014) do
+ActiveRecord::Schema.define(version: 20160202194539) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
